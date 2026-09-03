47964c2cbddbdc36ad83d1e28101e4ea1bb0423f
Konstantin Shvagerius <konstantinshvagerius@yandex.ru>
2026-09-03 22:57:48 +0300

    film-pgql-reviwe: добавлена статика + orderModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,12 +1,19 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Film } from './entities/film.entity';
 import { Schedule } from './entities/schedule.entity';
 import { FilmsModule } from './films/films.module';
+import { OrderModule } from './order/dto/order.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+      serveRoot: '/',
+    }),
     ConfigModule.forRoot(),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
@@ -22,8 +29,7 @@ import { FilmsModule } from './films/films.module';
       }),
     }),
     FilmsModule,
+    OrderModule,
   ],
-  
-  
 })
 export class AppModule {}
